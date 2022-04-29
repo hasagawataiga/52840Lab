0d0e5923801ee85834897e83bfd996a78c4c59c6
Tuan Vo Le Hoang <64889084+hasagawataiga@users.noreply.github.com>
2022-04-29 19:11:49 +0200

    Initialize source code

---
+++ Spring Challenge 2022/SourceCode.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int baseX = in.nextInt(); // The corner of the map representing your base
+        int baseY = in.nextInt();
+        int heroesPerPlayer = in.nextInt(); // Always 3
+
+        // game loop
+        while (true) {
+            for (int i = 0; i < 2; i++) {
+                int health = in.nextInt(); // Each player's base health
+                int mana = in.nextInt(); // Ignore in the first league; Spend ten mana to cast a spell
+            }
+            int entityCount = in.nextInt(); // Amount of heros and monsters you can see
+            for (int i = 0; i < entityCount; i++) {
+                int id = in.nextInt(); // Unique identifier
+                int type = in.nextInt(); // 0=monster, 1=your hero, 2=opponent hero
+                int x = in.nextInt(); // Position of this entity
+                int y = in.nextInt();
+                int shieldLife = in.nextInt(); // Ignore for this league; Count down until shield spell fades
+                int isControlled = in.nextInt(); // Ignore for this league; Equals 1 when this entity is under a control spell
+                int health = in.nextInt(); // Remaining health of this monster
+                int vx = in.nextInt(); // Trajectory of this monster
+                int vy = in.nextInt();
+                int nearBase = in.nextInt(); // 0=monster with no target yet, 1=monster targeting a base
+                int threatFor = in.nextInt(); // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+            }
+            for (int i = 0; i < heroesPerPlayer; i++) {
+
+                // Write an action using System.out.println()
+                // To debug: System.err.println("Debug messages...");
+
+
+                // In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
+                System.out.println("WAIT");
+            }
+        }
+    }
+}
